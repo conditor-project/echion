f5c2be7ee85ed4c337a35341ca67aa59c4b4aee8
Rémy MEJA <remymeja@free.fr>
2019-10-01 22:11:25 +0200

    add form validation

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ import './src/venn-diagram.js';
 class MyApp extends window.HTMLElement {
   connectedCallback () {
     render(this.template(), this, { eventContext: this });
+    const form = this.querySelector('form');
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+      event.stopPropagation();
+      if (form.checkValidity()) this.injectData();
+      form.classList.add('was-validated');
+    }, false);
   }
 
   template () {
@@ -12,24 +19,23 @@ class MyApp extends window.HTMLElement {
     <form novalidate>
       <div class="form-group">
         <label for="JSONFileOne">Choose source A</label>
-        <my-input-file type="file" class="form-control-file" id="JSONFileOne"></my-input-file>
+        <my-input-file id="JSONFileOne"></my-input-file>
       </div>
       <div class="form-group">
         <label for="JSONFileTwo">Choose source B</label>
-        <my-input-file type="file" class="form-control-file" id="JSONFileTwo"></my-input-file>
+        <my-input-file id="JSONFileTwo"></my-input-file>
       </div>
       <div class="form-group">
         <label for="commonField">Common field</label>
-        <input type="text" class="form-control" id="commonField" placeholder="Enter a field's name">
+        <input type="text" class="form-control" id="commonField" placeholder="Enter a field's name" required>
       </div>
-      <button class="btn btn-primary" @click=${this.onSubmit}>Submit</button>
+      <button class="btn btn-primary">Submit</button>
     </form>
     <venn-diagram></venn-diagram>
     `;
   }
 
-  onSubmit (event) {
-    event.preventDefault();
+  injectData () {
     const inputFiles = [].slice.call(this.querySelectorAll('my-input-file'));
     const dataFiles = inputFiles.map(inputFile => {
       return ({ data: inputFile.data, name: inputFile.name });
